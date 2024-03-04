7dd4909bf93088589d1a2581b892754904a870fd
Subhom Ghosal <152959262+iamsubhom@users.noreply.github.com>
2024-03-04 09:18:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,8 +33,8 @@ st.title('AI vs Real Image Classification')
 st.write('Sample Images to Test' )
 st.image('real1.jpg', caption='Real Photo 1')
 st.image('real2.jpg', caption='Real Photo 2')
-st.image('ai1.jpg', caption='Ai Generated Photo 1')
-st.image('ai2.jpg', caption='Ai Generated Photo 2')
+st.image('Ai1.jpg', caption='Ai Generated Photo 1')
+st.image('Ai2.jpg', caption='Ai Generated Photo 2')
 uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 
